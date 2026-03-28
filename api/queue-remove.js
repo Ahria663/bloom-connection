8db77aff6041da5b94c17078327cd320d1ef99b7
Baria Mustafa <Ahriabaria663@gmail.com>
2026-03-28 12:34:08 -0400

    fix API files - convert to CommonJS module.exports

--- api/queue-remove.js
+++ api/queue-remove.js
@@ -1,7 +1,7 @@
 // api/queue-remove.js
 // Deletes a played song from bloom_queue, scoped to a session.
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
--- api/search.js
+++ api/search.js
@@ -44,7 +44,7 @@ async function getSessionRefreshToken(sessionId) {
   return rows[0].refresh_token;
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   if (req.method === 'OPTIONS') return res.status(200).end();
