87912fab9d7ae1c06e06d40ba135991feb012082
Baria Mustafa <Ahriabaria663@gmail.com>
2026-03-27 20:50:51 -0400

    updateds

--- api/server.js
+++ api/server.js
@@ -172,4 +172,11 @@ if (!process.env.VERCEL) {
   });
 }
 
-module.exports = server;
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+module.exports = app;
+
+
